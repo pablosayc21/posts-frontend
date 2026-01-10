3401ea55b1caad950c855cfbf6aa629e870d4c9e
pablosayc21 <pablosayc@gmail.com>
2026-01-09 20:21:11 -0600

    feat(rutas): agrega la página principal al ruteo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '', 
+        loadComponent: () => 
+            import('./features/posts/pages/posts-list/posts-list.component').then(m => m.PostsListComponent)
+    }
+];
