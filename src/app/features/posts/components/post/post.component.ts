a46800a1c991aa98d05e1680356975943688639c
pablosayc21 <pablosayc@gmail.com>
2026-01-09 21:50:09 -0600

    feat(posts): agrega componente para posts individuales

---
+++ src/app/features/posts/components/post/post.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Post } from '../../models/post.interface';
+import { CommonModule } from '@angular/common';
+import { FechaPipe } from '../../../../shared/pipes/fecha/fecha.pipe';
+import { NgClass } from '@angular/common';
+@Component({
+  selector: 'app-post',
+  imports: [NgClass, FechaPipe, CommonModule],
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.scss'
+})
+
+export class PostComponent {
+  
+  @Input({ required: true }) post!: Post;
+  @Input({required: true}) postColor: PostColor = 'teal';
+
+}
+
+type PostColor = 'teal' | 'green' | 'yellow' | string;
