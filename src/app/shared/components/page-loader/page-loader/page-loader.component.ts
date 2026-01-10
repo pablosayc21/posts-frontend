02d9e55d5062043f1b5c079ac5b4677b50e1c402
pablosayc21 <pablosayc@gmail.com>
2026-01-10 13:00:12 -0600

    feat(loader): agrega componente para mostrar que una página está cargando

---
+++ src/app/shared/components/page-loader/page-loader/page-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-page-loader',
+  imports: [LucideAngularModule],
+  templateUrl: './page-loader.component.html',
+  styleUrl: './page-loader.component.scss'
+})
+export class PageLoaderComponent {
+
+}
